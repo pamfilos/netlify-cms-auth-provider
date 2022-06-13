fb0fe489b339407b704798f4988c75274562c85b
pamfilos <pamfilosf@gmail.com>
2022-06-13 11:24:28 +0200

    update for oauth2
    Signed-off-by: pamfilos <pamfilosf@gmail.com>

--- server.js
+++ server.js
@@ -1,5 +1,77 @@
+// const Prometheus = require('prom-client')
+// const express = require('express');
+// const http = require('http');
+
+// Prometheus.collectDefaultMetrics();
+
+// const requestHistogram = new Prometheus.Histogram({
+//     name: 'http_request_duration_seconds',
+//     help: 'Duration of HTTP requests in seconds',
+//     labelNames: ['code', 'handler', 'method'],
+//     buckets: [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5]
+// })
+
+// const requestTimer = (req, res, next) => {
+//   const path = new URL(req.url, `http://${req.hostname}`).pathname
+//   const stop = requestHistogram.startTimer({
+//     method: req.method,
+//     handler: path
+//   })
+//   res.on('finish', () => {
+//     stop({
+//       code: res.statusCode
+//     })
+//   })
+//   next()
+// }
+
+// const app = express();
+// const server = http.createServer(app)
+
+// // See: http://expressjs.com/en/4x/api.html#app.settings.table
+// const PRODUCTION = app.get('env') === 'production';
+
+// // Administrative routes are not timed or logged, but for non-admin routes, pino
+// // overhead is included in timing.
+// app.get('/ready', (req, res) => res.status(200).json({status:"ok"}));
+// app.get('/live', (req, res) => res.status(200).json({status:"ok"}));
+// app.get('/metrics', (req, res, next) => {
+//   res.set('Content-Type', Prometheus.register.contentType)
+//   res.end(Prometheus.register.metrics())
+// })
+
+// // Time routes after here.
+// app.use(requestTimer);
+
+// // Log routes after here.
+// const pino = require('pino')({
+//   level: PRODUCTION ? 'info' : 'debug',
+// });
+// app.use(require('pino-http')({logger: pino}));
+
+// app.get('/', (req, res) => {
+//   // Use req.log (a `pino` instance) to log JSON:
+//   req.log.info({message: 'Hello from Node.js Starter Application!'});
+//   res.send('Hello from Node.js Starter Application!');
+// });
+
+// app.get('*', (req, res) => {
+//   res.status(404).send("Not Found");
+// });
+
+// // Listen and serve.
+// const PORT = process.env.PORT || 3000;
+// server.listen(PORT, () => {
+//   console.log(`App started on PORT ${PORT}`);
+// });
+
+// 
+// 
+// 
 const Prometheus = require('prom-client')
-const express = require('express');
+require('dotenv').config({ silent: true })
+const express = require('express')
+const middleWarez = require('./index.js')
 const http = require('http');
 
 Prometheus.collectDefaultMetrics();
@@ -25,7 +97,9 @@ const requestTimer = (req, res, next) => {
   next()
 }
 
-const app = express();
+const port = process.env.PORT || 3000
+
+const app = express()
 const server = http.createServer(app)
 
 // See: http://expressjs.com/en/4x/api.html#app.settings.table
@@ -49,6 +123,17 @@ const pino = require('pino')({
 });
 app.use(require('pino-http')({logger: pino}));
 
+
+// Initial page redirecting to Github
+app.get('/auth', middleWarez.auth)
+
+// Callback service parsing the authorization token
+// and asking for the access token
+app.get('/callback', middleWarez.callback)
+
+app.get('/success', middleWarez.success)
+app.get('/', middleWarez.index)
+
 app.get('/', (req, res) => {
   // Use req.log (a `pino` instance) to log JSON:
   req.log.info({message: 'Hello from Node.js Starter Application!'});
@@ -58,7 +143,9 @@ app.get('/', (req, res) => {
 app.get('*', (req, res) => {
   res.status(404).send("Not Found");
 });
-
+// app.listen(port, () => {
+//   console.log("gandalf is walkin' on port " + port)
+// })
 // Listen and serve.
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
